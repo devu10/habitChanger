45ab620851e6f80db3ddc4df36690c6ec2b96eb2
Devu <devudevu10@gmail.com>
2024-06-26 21:29:44 +1000

    html converted with css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,85 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      {" "}
+      <div class="wrapper pt-5">
+        {/* <!-- title --> */}
+        <div class="container">
+          <h1 class="text-center">Habit List</h1>
+          {/* <!-- form --> */}
+          <form
+            onsubmit="formSubmit(this)"
+            action="javascript:void(0)"
+            class="border p-5 rounded shadow-lg mt-5"
+          >
+            <div class="row">
+              <div class="col-md-7">
+                <input
+                  type="text"
+                  id="habits"
+                  class="form-control"
+                  placeholder="Habit"
+                  aria-label="First name"
+                  name="habit"
+                />
+              </div>
+              <div class="col-md-2">
+                <input
+                  id="hrs"
+                  type="number"
+                  class="form-control"
+                  placeholder="Hour"
+                  aria-label="Last name"
+                  name="hr"
+                  min="1"
+                />
+              </div>
+              <div class="col-md-3 d-flex gap-2">
+                <button type="submit" class="btn btn-primary">
+                  Add New Habit
+                </button>
+                <div type="clear" class="btn btn-primary" onclick="clearForm()">
+                  clear
+                </div>
+              </div>
+            </div>
+          </form>
 
-  return <>hello</>;
+          {/* <!-- tables --> */}
+          <div class="row mt-5">
+            <div class="col-md text-center">
+              <h3>Habits</h3>
+              <hr />
+              {/* <!-- habit tables --> */}
+              <table class="table table-striped table-hover">
+                <tbody id="habitList"></tbody>
+              </table>
+            </div>
+            <div class="col-md text-center">
+              <h3>Habits to Improve</h3>
+              <hr />
+              {/* <!-- to improve habits tables --> */}
+              <table class="table table-striped table-hover">
+                <tbody id="improveList"></tbody>
+              </table>
+              <div class="alert alert-success">
+                You could have saved <span id="svdHr">0</span> hours
+              </div>
+            </div>
+          </div>
+          {/* <!-- displa --> */}
+          <div class="alert alert-success">
+            The total hours allocated =<span id="tlHr"> 0</span>
+          </div>
+        </div>
+      </div>
+      <footer class="bg-black text-center text-white">
+        &copy devudevu10@gmail.com
+      </footer>
+    </>
+  );
 }
 
 export default App;
