177ec67dd645eade99c3d0070912d6c48e1cf88a
Devu <devudevu10@gmail.com>
2024-06-30 22:02:05 +1000

    html migrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
-const hrPerWeek = 24 * 7;
 
 function App() {
-  const [tlHr, setTlHr] = useState("");
-  const [svdHr, setSvdHr] = useState("");
-  const [habits, sethabits] = useState("");
-  const [hrs, sethrs] = useState("");
-  const [habitList, setHabitList] = useState([]);
-  const formSubmit = (e) => {
-    e.preventDefault();
-    const newForm = new FormData(e.target);
-    const habit = newForm.get("habit");
-    const hr = +newForm.get("hr");
-    const obj = {
-      habit,
-      hr,
-      id: idGen(),
-      type: "g",
-    };
-
-    const currentHr = totalHr();
-
-    if (currentHr + hr > hrPerWeek) {
-      return alert("total hr cannot exceed the total hrs ina week");
-    }
-
-    setHabitList((prev) => {
-      const updatedList = [...prev, obj];
-      setTlHr(totalHr(updatedList));
-      calculateSavedHours(updatedList);
-      return updatedList;
-    });
-
-    clearForm();
-  };
-  const idGen = (length = 6) => {
-    const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPSDFGHJKLZXCVBNM123456789";
-
-    let id = "";
-
-    for (let i = 0; i < 6; i++) {
-      const index = Math.floor(Math.random() * str.length);
-      id += str[index];
-    }
-
-    return id;
-  };
-  const totalHr = () => {
-    const tothr = habitList.reduce((acc, item) => {
-      return acc + item.hr;
-    }, 0);
-    setTlHr(tothr);
-    return tothr;
-  };
-
-  const calculateSavedHours = () => {
-    const savedHours = habitList
-      .filter((item) => item.type === "b")
-      .reduce((acc, item) => acc + item.hr, 0);
-    setSvdHr(savedHours);
-  };
-
-  const onDelete = (id) => {
-    if (window.confirm("Are you sure you want to delete?")) {
-      setHabitList((prev) => {
-        const updatedList = prev.filter((item) => item.id !== id);
-        setTlHr(totalHr(updatedList));
-        calculateSavedHours(updatedList);
-        return updatedList;
-      });
-    }
-  };
-
-  const moveHabit = (id, type) => {
-    setHabitList((prev) => {
-      const updatedList = prev.map((item) => {
-        if (item.id === id) {
-          return { ...item, type };
-        }
-        return item;
-      });
-      setTlHr(totalHr(updatedList));
-      calculateSavedHours(updatedList);
-      return updatedList;
-    });
-  };
-
-  const clearForm = () => {
-    sethabits("");
-    sethrs("");
-  };
   return (
     <>
       <div className="wrapper pt-5">
@@ -101,34 +10,29 @@ function App() {
           <h1 className="text-center">Habit List</h1>
           {/* <!-- form --> */}
           <form
-            onSubmit={formSubmit}
+            onsubmit="formSubmit(this)"
+            action="javascript:void(0)"
             className="border p-5 rounded shadow-lg mt-5"
           >
             <div className="row">
               <div className="col-md-7">
                 <input
                   type="text"
+                  id="habits"
                   className="form-control"
                   placeholder="Habit"
                   aria-label="First name"
                   name="habit"
-                  value={habits}
-                  onChange={(e) => {
-                    sethabits(e.target.value);
-                  }}
                 />
               </div>
               <div className="col-md-2">
                 <input
+                  id="hrs"
                   type="number"
                   className="form-control"
                   placeholder="Hour"
                   aria-label="Last name"
                   name="hr"
-                  value={hrs}
-                  onChange={(e) => {
-                    sethrs(e.target.value);
-                  }}
                   min="1"
                 />
               </div>
@@ -137,13 +41,11 @@ function App() {
                   Add New Habit
                 </button>
                 <div
+                  type="clear"
                   className="btn btn-primary"
-                  onClick={() => {
-                    sethabits("");
-                    sethrs("");
-                  }}
+                  onclick="clearForm()"
                 >
-                  Clear
+                  clear
                 </div>
               </div>
             </div>
@@ -156,31 +58,7 @@ function App() {
               <hr />
               {/* <!-- habit tables --> */}
               <table className="table table-striped table-hover">
-                <tbody>
-                  {habitList
-                    .filter((item) => item.type === "g")
-                    .map((item, i) => (
-                      <tr key={item.id}>
-                        <td>{i + 1}</td>
-                        <td>{item.habit}</td>
-                        <td>{item.hr}</td>
-                        <td className="text-end">
-                          <button
-                            onClick={() => onDelete(item.id)}
-                            className="btn btn-danger"
-                          >
-                            <i className="fa-solid fa-trash"></i>
-                          </button>
-                          <button
-                            onClick={() => moveHabit(item.id, "b")}
-                            className="btn btn-success"
-                          >
-                            <i className="fa-solid fa-arrow-right"></i>
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
+                <tbody id="habitList"></tbody>
               </table>
             </div>
             <div className="col-md text-center">
@@ -188,40 +66,16 @@ function App() {
               <hr />
               {/* <!-- to improve habits tables --> */}
               <table className="table table-striped table-hover">
-                <tbody>
-                  {habitList
-                    .filter((item) => item.type === "b")
-                    .map((item, i) => (
-                      <tr key={item.id}>
-                        <td>{i + 1}</td>
-                        <td>{item.habit}</td>
-                        <td>{item.hr}</td>
-                        <td className="text-end">
-                          <button
-                            onClick={() => moveHabit(item.id, "g")}
-                            className="btn btn-warning"
-                          >
-                            <i className="fa-solid fa-arrow-left"></i>
-                          </button>
-                          <button
-                            onClick={() => onDelete(item.id)}
-                            className="btn btn-danger"
-                          >
-                            <i className="fa-solid fa-trash"></i>
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
+                <tbody id="improveList"></tbody>
               </table>
               <div className="alert alert-success">
-                You could have saved <span id="svdHr">{svdHr}</span> hours
+                You could have saved <span id="svdHr">0</span> hours
               </div>
             </div>
           </div>
           {/* <!-- displa --> */}
           <div className="alert alert-success">
-            The total hours allocated =<span id="tlHr">{tlHr}</span>
+            The total hours allocated =<span id="tlHr"> 0</span>
           </div>
         </div>
       </div>
