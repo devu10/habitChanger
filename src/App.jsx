2de6550304dab5c9967b5002b1ca1b06a121a5a3
Devu <devudevu10@gmail.com>
2024-07-02 21:12:20 +1000

    table list added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form";
+import { Tables } from "./components/Tables";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
@@ -35,27 +36,7 @@ function App() {
           <Form addTaskList={addTaskList} />
 
           {/* <!-- tables --> */}
-          <div className="row mt-5">
-            <div className="col-md text-center">
-              <h3>Habits</h3>
-              <hr />
-              {/* <!-- habit tables --> */}
-              <table className="table table-striped table-hover">
-                <tbody id="habitList"></tbody>
-              </table>
-            </div>
-            <div className="col-md text-center">
-              <h3>Habits to Improve</h3>
-              <hr />
-              {/* <!-- to improve habits tables --> */}
-              <table className="table table-striped table-hover">
-                <tbody id="improveList"></tbody>
-              </table>
-              <div className="alert alert-success">
-                You could have saved <span id="svdHr">0</span> hours
-              </div>
-            </div>
-          </div>
+          <Tables taskList={taskList} />
           {/* <!-- displa --> */}
           <div className="alert alert-success">
             The total hours allocated =<span id="tlHr"> 0</span>
